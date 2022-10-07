85c46f717d384d0fc252023e472148cc8fcb6d2b
Jamie <jamiehanson2012@gmail.com>
2022-10-07 15:01:10 +0100

    update js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,18 @@
-hello
+//computer selects either rock paper or scissors
+//the player selects rock paper or scissors
+//the players selection is compared to computer selection
+//the winner is displayed in a string
+
+//computer choosing
+//player choosing
+//if statement to establish winner
+//declare winner
+
+const choice = ['Rock', 'Paper', 'Scissors'];
+
+
+function getComputerChoice(choice){
+    return choice[Math.floor(Math.random()*choice.length)];
+}
+console.log(getComputerChoice(choice));
+
