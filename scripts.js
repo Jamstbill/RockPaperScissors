8d78998d347f76aa5b72b4b1dcd6d2ed68b7e884
Jamie <jamiehanson2012@gmail.com>
2024-06-16 11:00:49 +0100

    Added functionality to play the full game

--- scripts.js
+++ scripts.js
@@ -1,12 +1,20 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
     let computerChoice;
     let randomNumber = Math.floor(Math.random() * 3) + 1;
-    if (randomNumber == 1){
-        computerChoice = 'Rock';
-    } else if (randomNumber == 2){
-        computerChoice = 'Scissors';
-    } else {
-        computerChoice = 'Paper';
+
+    switch(randomNumber){
+        case 1:
+            computerChoice = 'Rock';
+            break;
+        case 2:
+            computerChoice = 'Scissors';
+            break;
+        case 3:
+            computerChoice = 'Paper';
+            break;
     }
     return computerChoice.toUpperCase();
 }
@@ -16,44 +24,85 @@ function getPlayerChoice(){
     return playerChoice.toUpperCase();
 }
 
-let playerScore = 0;
-let computerScore = 0;
-let computerChoice = getComputerChoice();
-let playerChoice = getPlayerChoice();
 
 function playRound(playerChoice, computerChoice){
     if (playerChoice == 'ROCK' && computerChoice == 'PAPER'){
         console.log('You lose! Paper beats Rock');
-        computerScore = computerScore + 1;
-        console.log(`Your score: ${playerScore}. Computer score: ${computerScore}`);
+        return 'computerWin';
     } else if (playerChoice == 'ROCK' && computerChoice == 'SCISSORS'){
         console.log('You win! Rock beats Scissors');
-        playerScore = playerScore + 1;
-        console.log(`Your score: ${playerScore}. Computer score: ${computerScore}`);
+        return 'playerWin';
     } else if (playerChoice == 'PAPER' && computerChoice == 'ROCK'){
         console.log('You win! Paper beats Rock');
-        playerScore = playerScore + 1;
-        console.log(`Your score: ${playerScore}. Computer score: ${computerScore}`);
+        return 'playerWin';
     } else if (playerChoice == 'PAPER' && computerChoice == 'SCISSORS'){
         console.log('You lose! Scissors beats Paper');
-        computerScore = computerScore + 1;
-        console.log(`Your score: ${playerScore}. Computer score: ${computerScore}`);
+        return 'computerWin';
     } else if (playerChoice == 'SCISSORS' && computerChoice == 'ROCK'){
         console.log('You lose! Rock beats Scissors');
-        computerScore = computerScore + 1;
-        console.log(`Your score: ${playerScore}. Computer score: ${computerScore}`);
+        return 'computerWin';
     } else if (playerChoice == 'SCISSORS' && computerChoice == 'PAPER'){
         console.log('You win! Scissors beats Paper');
-        playerScore = playerScore + 1;
-        console.log(`Your score: ${playerScore}. Computer score: ${computerScore}`);
+        return 'playerWin';
     } else if(playerChoice == computerChoice){
         console.log('Its a draw!')
-        console.log(`Your score: ${playerScore}. Computer score: ${computerScore}`);
     } else{
-        console.log('error');
+        console.log('Please enter Rock, Paper or Scissors');
     }
 }
-playRound(playerChoice, computerChoice);
-//function to play the game 5 times
-//needs to call the playround function 5 times
-//needs to update the scores per round
+
+function checkRoundWinner(result){
+    if (result == 'playerWin'){
+        playerScore = playerScore + 1;
+    } else if (result == 'computerWin'){
+        computerScore = computerScore + 1;
+    } 
+    console.log(`Your score: ${playerScore}. Computer score: ${computerScore}`);
+    console.log('||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||');
+}
+
+
+const gameWinner = (playerScore, computerScore) => {
+    if (playerScore > computerScore){
+        console.log('||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||');
+        console.log('CONGRATULATIONS! YOU WIN!')
+    } else if(playerScore < computerScore){
+        console.log('||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||');
+        console.log('YOU LOSE! COMPUTER WINS!')
+    } else{
+        console.log('||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||');
+        console.log('ITS A DRAW');
+    }
+}
+
+function playGame(){
+    let round = 1;
+    console.log(`Round number: ${round}`);
+
+    let result = playRound(getPlayerChoice(), getComputerChoice());
+    checkRoundWinner(result);
+
+    round = round + 1;
+    result = playRound(getPlayerChoice(), getComputerChoice());
+    console.log(`Round number: ${round}`);
+    checkRoundWinner(result);
+
+    round = round + 1;
+    result = playRound(getPlayerChoice(), getComputerChoice());
+    console.log(`Round number: ${round}`);
+    checkRoundWinner(result);
+
+    round = round + 1;
+    result = playRound(getPlayerChoice(), getComputerChoice());
+    console.log(`Round number: ${round}`);
+    checkRoundWinner(result);
+
+    round = round + 1;
+    result = playRound(getPlayerChoice(), getComputerChoice());
+    console.log(`Round number: ${round}`);
+    checkRoundWinner(result);
+
+    gameWinner(playerScore, computerScore);
+}
+
+playGame();
